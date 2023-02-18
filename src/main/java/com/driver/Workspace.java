3beec3804b15adda2f7cd675299e9f1805e6f17c
zebatab <zebatab094@gmail.com>
2023-02-19 00:04:54 +0530

    second commit

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -12,9 +12,9 @@ public class Workspace extends Gmail{
 
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
-        super(emailId);
+        super(emailId, Integer.MAX_VALUE);
         calendar = new ArrayList<>();
-        inboxCapacity(Integer.MAX_VALUE);
+        //inboxCapacity(Integer.MAX_VALUE);
 
     }
 
